b003b047ee51fed9ca30ea333d4876d6f3efe751
Elizabeth Reuter <elizabeth.j.reuter@gmail.com>
2020-11-12 15:56:53 -0600

    fixed workout dashboard showing up

--- routes/api.js
+++ routes/api.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const Workouts = require("../models/workout.js");
 
 // adds exercises to previous workout plan
-router.post("/api/workouts/", ({ body }, res) => {
+router.post("/api/workouts", ({ body }, res) => {
   Workouts.create(body)
     .then(response => {
       res.json(response);
@@ -11,7 +11,19 @@ router.post("/api/workouts/", ({ body }, res) => {
       res.status(400).json(err);
     });
 });
-//adds new exercises to a new workout plan
+
+router.post("/api/addExercise/:id", (req, res) => {
+// tell the db that we want to get the workout of the id (req.params.id or req.id? rec.body.id?) and then add to its exercises what we have in req.body
+    Workouts.find({})
+    // ^^ in find we need to tell it what to find in the {id:req.body.id}
+    .then(response => {
+    // want to take this response and parse it into the workouts object (ex.... workout.exercises.push/add(req.body), and then update to the db
+    })
+    .catch(err => {
+    res.status(400).json(err);
+    });
+    console.log(req.body);
+  });
 
 
 // View the combined weight of multiple exercises on stats page
